e2c665927ff6f548f9273fe99742f942ee96aaa7
Andnet DeBoer <deboerandnet@gmail.com>
2026-06-03 22:43:14 -0500

    add piper to replay script

--- src/lerobot/scripts/lerobot_replay.py
+++ src/lerobot/scripts/lerobot_replay.py
@@ -64,6 +64,7 @@ from lerobot.robots import (  # noqa: F401
     make_robot_from_config,
     omx_follower,
     openarm_follower,
+    piper,
     reachy2,
     rebot_b601_follower,
     so_follower,
